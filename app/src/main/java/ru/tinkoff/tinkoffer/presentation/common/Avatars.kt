12a41824580c73b8d82d5464ecaf0b538b1d3f0e
Tit Tolkachev <tittolkachev@gmail.com>
2024-04-21 02:43:19 +0700

    Аватарка Егора.

--- app/src/main/java/ru/tinkoff/tinkoffer/presentation/common/Avatars.kt
+++ app/src/main/java/ru/tinkoff/tinkoffer/presentation/common/Avatars.kt
@@ -3,16 +3,16 @@ package ru.tinkoff.tinkoffer.presentation.common
 import ru.tinkoff.tinkoffer.R
 
 val avatars = listOf(
-    R.drawable.ic_launcher_foreground,
-    R.drawable.ic_launcher_foreground,
-    R.drawable.ic_launcher_foreground,
-    R.drawable.ic_launcher_foreground,
-    R.drawable.ic_launcher_foreground,
-    R.drawable.ic_launcher_foreground,
-    R.drawable.ic_launcher_foreground,
-    R.drawable.ic_launcher_foreground,
-    R.drawable.ic_launcher_foreground,
-    R.drawable.ic_launcher_foreground,
-    R.drawable.ic_launcher_foreground,
-    R.drawable.ic_launcher_foreground,
+    R.drawable.egor,
+    R.drawable.egor,
+    R.drawable.egor,
+    R.drawable.egor,
+    R.drawable.egor,
+    R.drawable.egor,
+    R.drawable.egor,
+    R.drawable.egor,
+    R.drawable.egor,
+    R.drawable.egor,
+    R.drawable.egor,
+    R.drawable.egor,
 )
