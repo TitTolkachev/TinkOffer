bbcb068f872545f7044e199eb0dfa76a8076a69e
Tit Tolkachev <tittolkachev@gmail.com>
2024-04-21 04:02:45 +0700

    Аватарки.

--- app/src/main/java/ru/tinkoff/tinkoffer/presentation/common/Avatars.kt
+++ app/src/main/java/ru/tinkoff/tinkoffer/presentation/common/Avatars.kt
@@ -3,16 +3,17 @@ package ru.tinkoff.tinkoffer.presentation.common
 import ru.tinkoff.tinkoffer.R
 
 val avatars = listOf(
-    R.drawable.egor,
-    R.drawable.egor,
-    R.drawable.egor,
-    R.drawable.egor,
-    R.drawable.egor,
-    R.drawable.egor,
-    R.drawable.egor,
-    R.drawable.egor,
-    R.drawable.egor,
-    R.drawable.egor,
-    R.drawable.egor,
+    R.drawable.frame_1,
+    R.drawable.frame_2,
+    R.drawable.frame_3,
+    R.drawable.frame_4,
+    R.drawable.frame_5,
+    R.drawable.frame_6,
+    R.drawable.frame_7,
+    R.drawable.frame_8,
+    R.drawable.frame_9,
+    R.drawable.frame_10,
+    R.drawable.frame_11,
+    R.drawable.frame_12,
     R.drawable.egor,
 )
