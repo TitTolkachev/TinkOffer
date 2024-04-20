92f027d47f54194097cf044051ded62da1d93e5f
Tit Tolkachev <tittolkachev@gmail.com>
2024-04-21 01:03:12 +0700

    Fixed StatusBar.

--- app/src/main/java/ru/tinkoff/tinkoffer/presentation/theme/Theme.kt
+++ app/src/main/java/ru/tinkoff/tinkoffer/presentation/theme/Theme.kt
@@ -1,16 +1,11 @@
 package ru.tinkoff.tinkoffer.presentation.theme
 
-import android.app.Activity
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.toArgb
-import androidx.compose.ui.platform.LocalView
-import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
     primary = Color(0xFFFFDD2D),
@@ -51,14 +46,6 @@ fun AppTheme(
     val colorScheme = when {
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
-    }
-    val view = LocalView.current
-    if (!view.isInEditMode) {
-        SideEffect {
-            val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.surface.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
-        }
     }
 
     MaterialTheme(
